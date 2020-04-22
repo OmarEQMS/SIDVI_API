1bfbd9fd609723692e4e35351c325db81972d5e4
unknown <a01421467@itesm.mx>
2020-04-22 00:56:26 -0500

    add user i seeds

--- test/seeds/Usuario.Seed.ts
+++ test/seeds/Usuario.Seed.ts
@@ -9,6 +9,7 @@ import { Defaults, ContentTypeEnum } from '../../source/api';
 export const seed = async (knex: Knex) => {
 	await knex('Usuario').insert([
 		{ idUsuario:1, nombreCompleto: 'Omar Quintero', usuario: 'omar', contrasena: await bcrypt.hash('1997', Defaults.saltRounds), token: '1997', mimetypeFoto: ContentTypeEnum.JPG, correo:'omar.quintero@omar.com', celular:'7771321625', archivoFoto: fs.readFileSync(path.resolve(__dirname, '..', 'files', 'perfilOmar.jpg')), rol:_Usuario.Rol.ADMINISTRADOR } as IUsuario,
+		{ idUsuario:2, nombreCompleto: 'Angelica Guemes', usuario: 'angie', contrasena: await bcrypt.hash('123', Defaults.saltRounds), token: '123', mimetypeFoto: ContentTypeEnum.JPG, correo:'angieguemes@gmail.com', celular:'7771321625', archivoFoto: fs.readFileSync(path.resolve(__dirname, '..', 'files', 'angieImg.jpg')), rol:_Usuario.Rol.USUARIO } as IUsuario,
     ]);
 	return knex;
 };
